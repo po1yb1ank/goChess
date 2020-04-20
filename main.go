0c4cc3cd3b92409f1257eb5935bec153b0819a35
Daniil Davydov <b00t.b00t@yandex.ru>
2020-04-20 17:19:22 +0300

    file upload

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import(
+	"io/ioutil"
 	"net/http"
 	"fmt"
 )
 func uploadFile(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Uploading file")
+	fmt.Fprintf(w, "Uploading file\n")
 	//parse input
 	r.ParseMultipartForm(10 << 20) //20Mb
-	//retrieve file from posted form-adta
-	//write temp file onto server
+	//retrieve file from posted form-data
 	//retun code
-	file, handler, err := r.FormFile("MyFile")
+	file, handler, err := r.FormFile("File")
 	if err != nil	{
 		fmt.Println("Error retrieving file from form-data")
 		fmt.Println(err)
@@ -21,6 +21,25 @@ func uploadFile(w http.ResponseWriter, r *http.Request){
 	fmt.Printf("Upload File: %+v\n",handler.Filename)
 	fmt.Printf("File size: %+v\n",handler.Size)
 	fmt.Printf("MIME header: %+v\n",handler.Header)
+
+	//write temp file onto server
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil{
+		fmt.Println("Uploading error")
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tempFile.Write(fileBytes)
+
+	fmt.Fprintf(w, "Successfully uploaded file\n")
+
 }
 func SetupRoutes()	{
 	http.HandleFunc("/upload", uploadFile)
