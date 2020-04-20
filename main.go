4e8715006b74881db5a6665ff8f27b26ac5fe9aa
Daniil Davydov <b00t.b00t@yandex.ru>
2020-04-20 16:52:09 +0300

    parse input&retrieve file

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import(
 )
 func uploadFile(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Uploading file")
+	//parse input
+	r.ParseMultipartForm(10 << 20) //20Mb
+	//retrieve file from posted form-adta
+	//write temp file onto server
+	//retun code
+	file, handler, err := r.FormFile("MyFile")
+	if err != nil	{
+		fmt.Println("Error retrieving file from form-data")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Upload File: %+v\n",handler.Filename)
+	fmt.Printf("File size: %+v\n",handler.Size)
+	fmt.Printf("MIME header: %+v\n",handler.Header)
 }
 func SetupRoutes()	{
 	http.HandleFunc("/upload", uploadFile)
@@ -13,6 +28,7 @@ func SetupRoutes()	{
 }
 func main() {
 	fmt.Println("File upload example")
+	SetupRoutes()
 }
 
 
