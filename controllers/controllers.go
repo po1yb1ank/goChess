fc03da765b3377e655eac599daeb75fd9c2f4cc6
Daniil Davydov <b00t.b00t@yandex.ru>
2020-06-20 01:39:01 +0300

    size restrictions for login&passwd

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -73,7 +73,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("login: ", l)
 			fmt.Println("pass: ", p)
 			if len(l) > 4 && len(p) > 6 {
-				if x, _ := database.SeekDB(l); x == "" {
+				if x, y := database.SeekDB(l); x == "" || y == "" {
 				database.SetUser(l, p)
 				/*if database.IfLogged() == true {
 					http.Redirect(w, r, "https://127.0.0.1:8080/redirect", 301)
